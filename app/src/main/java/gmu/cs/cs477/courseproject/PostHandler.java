ae987dbccfcf5e6c5a1895308db96c2c2c9381be
dramzy <dramzy@gmu.edu>
2015-11-15 12:54:04 -0500

    removing redundant files

--- app/src/main/java/gmu/cs/cs477/courseproject/PostHandler.java
+++
@@ -1,9 +0,0 @@
-package gmu.cs.cs477.courseproject;
-
-
-import java.util.ArrayList;
-
-public interface PostHandler {
-    public ArrayList<Post> getPosts();
-    public void addPost(String post);
-}
